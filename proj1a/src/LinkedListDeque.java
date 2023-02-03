f2822612c2739ae5e2aac773206eb03d90f2b22c
Aashi Parikh <aashi.parikh@berkeley.edu>
2023-02-02 22:02:03 -0800

    constructor done

--- proj1a/src/LinkedListDeque.java
+++ proj1a/src/LinkedListDeque.java
@@ -1,2 +1,69 @@
-package PACKAGE_NAME;public class LinkedListDeque {
+import java.util.List;
+
+public class LinkedListDeque<T> implements Deque<T> {
+    private class Node {
+        public T item;
+        public Node next;
+        public Node prev;
+        public Node(T i, Node n) {
+            item = i;
+            prev = n;
+            next = n;
+        }
+    }
+    private Node sentinel;
+    private int size;
+    public LinkedListDeque() {
+        sentinel = new Node(null, null);
+        size = 0;
+    }
+
+    public static void main(String[] args) {
+        Deque<Integer> lld = new LinkedListDeque<>();
+    }
+
+    @Override
+    public void addFirst(T x) {
+
+    }
+
+    @Override
+    public void addLast(T x) {
+
+    }
+
+    @Override
+    public List<T> toList() {
+        return null;
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return false;
+    }
+
+    @Override
+    public int size() {
+        return 0;
+    }
+
+    @Override
+    public T removeFirst() {
+        return null;
+    }
+
+    @Override
+    public T removeLast() {
+        return null;
+    }
+
+    @Override
+    public T get(int index) {
+        return null;
+    }
+
+    @Override
+    public T getRecursive(int index) {
+        return null;
+    }
 }
