e766c32b12b0db70481df5ba818dfee809f7cd9c
Aashi Parikh <aashi.parikh@berkeley.edu>
2023-02-21 23:01:08 -0800

    lld fixed (ad util removed)

--- proj1c/src/deque/LinkedListDeque.java
+++ proj1c/src/deque/LinkedListDeque.java
@@ -1,6 +1,5 @@
 package deque;
 
-import java.util.ArrayDeque;
 import java.util.Iterator;
 import java.util.List;
 import java.util.ArrayList;
@@ -25,7 +24,7 @@ public class LinkedListDeque<T> implements Deque<T> {
     }
 
     public static void main(String[] args) {
-
+        
     }
 
     /**
@@ -233,6 +232,30 @@ public class LinkedListDeque<T> implements Deque<T> {
             if (lld.size != this.size) {
                 return false;
             }
+            for (int i = 0; i < this.size; i++) {
+                if (get(i) != lld.get(i)) {
+                    return false;
+                }
+            }
+            return true;
+        }
+        if (o instanceof ArrayDeque ad) {
+            if (ad.size() != this.size) {
+                return false;
+            }
+            for (int i = 0; i < this.size; i++) {
+                if (get(i) != ad.get(i)) {
+                    return false;
+                }
+            }
+            return true;
+        }
+        return false;
+        /*
+        if (o instanceof LinkedListDeque lld) {
+            if (lld.size != this.size) {
+                return false;
+            }
             for (T x : this) {
                 if (!lld.contains(x)) {
                     return false;
@@ -261,6 +284,8 @@ public class LinkedListDeque<T> implements Deque<T> {
             return true;
         }
         return false;
+
+         */
     }
 
     @Override
