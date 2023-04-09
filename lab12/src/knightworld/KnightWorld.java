31a8e1bcd53a40e1faa9edbef66c871bd99e5252
Aashi Parikh <aashi.parikh@berkeley.edu>
2023-04-08 21:29:23 -0700

    complete besides secret word

--- lab12/src/knightworld/KnightWorld.java
+++ lab12/src/knightworld/KnightWorld.java
@@ -12,45 +12,57 @@ public class KnightWorld {
     private TETile[][] tiles;
     private int width;
     private int height;
-    // TODO: Add additional instance variables here
 
     public KnightWorld(int width, int height, int holeSize) {
         this.width = width;
         this.height = height;
         fillTiles(holeSize);
-        // TODO: Fill in this constructor and class, adding helper methods and/or classes as necessary to draw the
-        //  specified pattern of the given hole size for a window of size width x height. If you're stuck on how to
-        //  begin, look at the provided demo code!
     }
 
-    int rowType = 0;
-
     public TETile[][] fillTiles(int holeSize) {
-        tiles = new TETile[width][height];
-        for (int i = 0; i < width; i++) {
-            if (rowType > 4) { rowType = 0;}
-            for (int j = 0; j < height; j++) {
-                if (isHole(rowType, j)) {
+        int rowType = 0;
+        int rowTracker = holeSize;
+        tiles = new TETile[height][width];
+        for (int i = 0; i < height; i++) {
+            if (rowType > 4) {
+                rowType = 0;
+            }
+            int colType = 0;
+            int colTracker = holeSize;
+            for (int j = 0; j < width; j++) {
+                if (colType > 4) {
+                    colType = 0;
+                }
+                if (isHole(rowType, colType)) {
                    tiles[i][j] = Tileset.NOTHING;
                 } else {
                     tiles[i][j] = Tileset.LOCKED_DOOR;
                 }
+                colTracker--;
+                if (colTracker == 0) {
+                    colType++;
+                    colTracker = holeSize;
+                }
             }
-            rowType++;
+            rowTracker--;
+            if (rowTracker == 0) {
+                rowType++;
+                rowTracker = holeSize;
+            }
         }
         return tiles;
     }
 
-    public boolean isHole(int typeOfRow, int col) {
-        if (typeOfRow == 0 && (col % 10 == 3 || col % 10 == 8)) {
+    public boolean isHole(int typeOfRow, int typeOfCol) {
+        if (typeOfRow == 0 && typeOfCol == 3) {
             return true;
-        } else if (typeOfRow == 1 && (col == 0 || col % 5 == 0)) {
+        } else if (typeOfRow == 1 && typeOfCol == 0) {
             return true;
-        } else if (typeOfRow == 2 && (col % 10 == 2 || col % 10 == 7)) {
+        } else if (typeOfRow == 2 && typeOfCol == 2) {
             return true;
-        } else if (typeOfRow == 3 && (col % 10 == 4 || col % 10 == 9)) {
+        } else if (typeOfRow == 3 && typeOfCol == 4) {
             return true;
-        } else if (typeOfRow == 4 && (col % 10 == 1 || col % 10 == 6)) {
+        } else if (typeOfRow == 4 && typeOfCol == 1) {
             return true;
         }
         return false;
@@ -64,9 +76,9 @@ public class KnightWorld {
 
     public static void main(String[] args) {
         // Change these parameters as necessary
-        int width = 50;
-        int height = 30;
-        int holeSize = 2;
+        int width = 60;
+        int height = 40;
+        int holeSize = 3;
 
         KnightWorld knightWorld = new KnightWorld(width, height, holeSize);
 
